c4d0de4c0f4dba6a8e277183206e234a319fe63e
Thanh Dat Tran <trandat021197@gmail.com>
2018-12-07 22:56:30 +0700

    Add Problem 1 in Golang

---
+++ PE-1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+
+	for number := 0; number < 1000; number++ {
+		if number % 3 == 0 || number % 5 == 0 {
+			sum += number
+		}
+	}
+	
+	fmt.Println(sum)
+}

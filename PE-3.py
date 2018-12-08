0e4808e12890e38c6e0e2f27f59e54dddc290cfd
Thanh Dat Tran <trandat021197@gmail.com>
2018-12-08 20:41:52 +0700

    Optimize Problem 3 in Python

--- PE-3.py
+++ PE-3.py
@@ -6,13 +6,13 @@ def max_prime(n):
     d = 2
     while n > 1:
         while n % d == 0:
-            yield d
-            n = n // d
+            n = n // d  # n is integer
         d += 1
+    return d - 1
 
 
 def main():
-    print(max(list(max_prime(600851475143))))
+    print(max_prime(600851475143))
 
 
 if __name__ == '__main__':

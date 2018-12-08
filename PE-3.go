86a42287423a2391d53c0be8fd7414446abf2844
Thanh Dat Tran <trandat021197@gmail.com>
2018-12-08 20:46:42 +0700

    Add Problem 3 in Go

---
+++ PE-3.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func max_prime(number int) int {
+	var d int = 2
+	for number > 1 {
+		for number % d == 0 {
+			number = number / d
+		}
+		d++
+	}
+	d--
+	return d
+}
+
+func main() {
+	fmt.Println(max_prime(600851475143))
+}

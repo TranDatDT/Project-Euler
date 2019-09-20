6beb9f0b41ec16b9c2ddcbec3d3e21ded142871b
Thanh Dat Tran <trandat021197@gmail.com>
2019-09-20 13:43:09 +0700

    Add PE-5 in Go

---
+++ Go/PE-5/PE-5.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func gcd(a int, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+
+	return a
+}
+
+func main() {
+	n := 20
+
+	lcm := 1
+
+	for i := 2; i<=n; i++ {
+		lcm = (lcm * i) / gcd(lcm, i)
+	}
+
+	fmt.Println(lcm)
+}
